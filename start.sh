ad324a63a842e8455cb9c83b350ced9c5d0bab40
emuyia <emuyiahere@gmail.com>
2024-05-28 01:05:11 +0100

    start.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+aconnect -l
+
 # Start Pure Data in headless mode with ALSA MIDI
 /usr/bin/chrt -f 50 /usr/bin/pd -rt -nogui -alsa /home/pi/repos/emsys/main.pd &
 
 # Wait for Pd to start
 sleep 5
 
-# Connect MiniLab 3 to Pure Data
-aconnect 32:0 128:0
+# Connect MIDI Controller
+aconnect 20:0 128:0
 
-# Connect TM-1 to Pure Data
-aconnect 28:0 128:0
+# Connect TM-1 / MegaCMD
+aconnect 32:0 128:0
 
 # Wait for Pd to run indefinitely
 wait
