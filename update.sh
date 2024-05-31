f07448456b7664e6c33c344daa4add97fe5937ae
emuyia <emuyiahere@gmail.com>
2024-05-31 04:52:24 +0000

    no service

--- update.sh
+++ update.sh
@@ -5,14 +5,16 @@ cd ~/repos/emsys || { echo "Directory not found"; exit 1; }
 echo "Pulling latest changes from Git..."
 git pull origin dev
 
-echo "Copying emsys.service to system directory..."
-sudo cp emsys.service /etc/systemd/system/emsys.service
+# echo "Copying emsys.service to system directory..."
+# sudo cp emsys.service /etc/systemd/system/emsys.service
 
-echo "Reloading systemd daemon..."
-sudo systemctl daemon-reload
+# echo "Reloading systemd daemon..."
+# sudo systemctl daemon-reload
 
-echo "Restarting emsys.service..."
-sudo systemctl restart emsys.service
+# echo "Restarting emsys.service..."
+# sudo systemctl restart emsys.service
 
-echo "Current status of emsys.service:"
-sudo journalctl -u emsys.service -f
+# echo "Current status of emsys.service:"
+# sudo journalctl -u emsys.service -f
+
+sudo ./start.sh
