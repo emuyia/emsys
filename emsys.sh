fbad020ac3a4dcaf062ce530782c8e202008150e
june <emuyiahere@gmail.com>
2024-06-25 12:55:15 +0100

    add: cpu & clock indicator, more debug btns, latency calibration safety

--- emsys.sh
+++ emsys.sh
@@ -3,5 +3,5 @@
 # ln -s /home/patch/repos/emsys/emsys.service ~/.config/systemd/user/emsys.service && systemctl --user daemon-reload && systemctl --user enable emsys && systemctl --user start emsys
 
 ~/Applications/pdnext/bin/pd ~/repos/emsys/em.sys.pd &
-sleep 15
+sleep 5
 wmctrl -r :ACTIVE: -b add,maximized_vert,maximized_horz
