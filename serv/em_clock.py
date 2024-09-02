96d7dcceebf103891bcda06de5fd2b2ee98b3516
emuyia <emuyiahere@gmail.com>
2024-09-02 02:10:27 +0100

    refactor services & improve efficiency of clock

--- serv/em_clock.py
+++ serv/em_clock.py
@@ -7,20 +7,25 @@ class MidiClockGen:
     def __init__(self):
         self.shared_bpm = Value('i', 120)
         self._run_code = Value('i', 1)
+        self.pulse_rate = Value('d', 60.0 / (self.shared_bpm.value * 24))  # Pre-calculate pulse rate
         self.midi_process = None
 
+    def update_pulse_rate(self):
+        """Update pulse rate when BPM changes."""
+        self.pulse_rate.value = 60.0 / (self.shared_bpm.value * 24)
+
     @staticmethod
-    def _midi_clock_generator(out_port, bpm, run):
+    def _midi_clock_generator(out_port, pulse_rate, run):
         midi_output = mido.open_output(out_port)
         clock_tick = mido.Message('clock')
         while run.value:
-            pulse_rate = 60.0 / (bpm.value * 24)
             midi_output.send(clock_tick)
             t1 = perf_counter()
-            if bpm.value <= 3000:
-                sleep(pulse_rate * 0.8)
+
+            sleep(pulse_rate.value * 0.8)  # Sleep for 80% of the pulse rate duration
             t2 = perf_counter()
-            while (t2 - t1) < pulse_rate:
+
+            while (t2 - t1) < pulse_rate.value:
                 t2 = perf_counter()
 
     def launch_process(self, out_port):
@@ -28,7 +33,7 @@ class MidiClockGen:
             self.end_process()
         self._run_code.value = 1
         self.midi_process = Process(target=self._midi_clock_generator,
-                                    args=(out_port, self.shared_bpm, self._run_code))
+                                    args=(out_port, self.pulse_rate, self._run_code))
         self.midi_process.start()
 
     def end_process(self):
@@ -51,6 +56,9 @@ def midi_bpm_listener(mcg, in_port_name, cc_number=1):
             if msg.type == 'control_change' and msg.control == cc_number:
                 new_bpm = int(30 + (msg.value * (300 - 30) / 127))
                 mcg.shared_bpm.value = new_bpm
+                mcg.update_pulse_rate()  # Update pulse rate when BPM changes
+        
+        sleep(0.1)  # Sleep for 100 ms to reduce CPU load
 
 class MidiClockApp:
     def __init__(self, port_name, midi_in_port_name):
