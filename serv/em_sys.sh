415817d5a1d47d0103a313f93133e5854556d9a3
emuyia <emuyiahere@gmail.com>
2024-09-05 18:36:13 +0100

    attempt to help patch stability at boot, pt2

--- serv/em_sys.sh
+++ serv/em_sys.sh
@@ -5,4 +5,5 @@ export JACK_PROMISCUOUS_SERVER=jack
 export DISPLAY=:0
 
 # Start Pd
-/home/patch/Applications/pdnext/bin/pd -jack -rt -nogui /home/patch/repos/emsys/em.sys.pd
+/home/patch/Applications/pdnext/bin/pd -jack -rt -nogui /home/patch/repos/emsys/em_sys.pd
+#/home/patch/Applications/pdnext/bin/pd -jack -rt -nogui /home/patch/repos/emsys/experiments/test_midi.pd
