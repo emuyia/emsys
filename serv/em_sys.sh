2b5da9b1fb64b1f23865a496075b3d43719f3361
em <emuyiahere@gmail.com>
2024-09-06 00:08:50 +0100

    reimplment transport

--- serv/em_sys.sh
+++ serv/em_sys.sh
@@ -5,5 +5,5 @@ export JACK_PROMISCUOUS_SERVER=jack
 export DISPLAY=:0
 
 # Start Pd
-/home/patch/Applications/pdnext/bin/pd -jack -rt -nogui /home/patch/repos/emsys/em/main.pd
-#/home/patch/Applications/pdnext/bin/pd -jack -rt -nogui /home/patch/repos/emsys/experiments/test_midi.pd
+#/home/patch/Applications/pdnext/bin/pd -jack -rt -nogui /home/patch/repos/emsys/em/main.pd
+/home/patch/Applications/pdnext/bin/pd -jack -rt -nogui /home/patch/repos/emsys/dummy.pd
