e3d3e056b18a082ab1ae4af0dc1b7bf6502a4147
em <emuyiahere@gmail.com>
2024-09-11 01:06:40 +0100

    add ml3 ctl for seg seek

--- serv/em_sys.sh
+++ serv/em_sys.sh
@@ -5,5 +5,5 @@ export JACK_PROMISCUOUS_SERVER=jack
 export DISPLAY=:0
 
 # Start Pd
-#/home/patch/Applications/pdnext/bin/pd -jack -rt -nogui /home/patch/repos/emsys/em/main.pd
-/home/patch/Applications/pdnext/bin/pd -jack -rt -nogui /home/patch/repos/emsys/dummy.pd
+/home/patch/Applications/pdnext/bin/pd -jack -rt -nogui /home/patch/repos/emsys/main.pd
+#/home/patch/Applications/pdnext/bin/pd -jack -rt -nogui /home/patch/repos/emsys/dummy.pd
