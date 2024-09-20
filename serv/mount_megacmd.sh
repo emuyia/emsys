6a2a494ac4ba4c31aeb931bc47dc3c8d92bef434
em <emuyiahere@gmail.com>
2024-09-20 02:22:51 +0100

    add mount scripts

--- serv/mount_megacmd.sh
+++ serv/mount_megacmd.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-MOUNTPOINT=~/.mnt/patchbox_home
-REMOTE=patch@patchbox.local:/home/patch/
+MOUNTPOINT=~/.mnt/patchbox_megacmd
+REMOTE=patch@patchbox.local:/media/patch/
 LOGFILE=~/.mnt/mnt_patchbox.log
+DEVICE=/dev/sda1
 
 echo "$(date): Checking mount" >> $LOGFILE
 
 if [ "$1" == "-u" ]; then
-    echo "$(date): Unmounting $MOUNTPOINT" >> $LOGFILE
+    echo "$(date): Unmounting $MOUNTPOINT and device on remote" >> $LOGFILE
+    ssh patch@patchbox.local "udevil unmount $DEVICE" >> $LOGFILE 2>&1
     fusermount -u "$MOUNTPOINT" >> $LOGFILE 2>&1
     exit 0
 fi
@@ -18,3 +20,6 @@ if ! mountpoint -q "$MOUNTPOINT"; then
 else
     echo "$(date): Already mounted" >> $LOGFILE
 fi
+
+# SSH and mount the device on the remote system
+ssh patch@patchbox.local "udevil mount $DEVICE" >> $LOGFILE 2>&1
